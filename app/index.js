82bdbed14d2adb30733f1e69ad13039e78500ddf
maks <msZatoshka@yandex.ru>
2018-09-05 18:30:44 +0400

    add

--- app/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('../app');
-const pool = require('../bd');
-app.get('/', function (req, res) {
-    res.render('index');
-  });
-
-module.exports = app;
--- bd.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-const pool = mysql.createPool({
-    host            : 'localhost',
-    user            : 'root',
-    password        : 'secret',
-    database        : 'node_js'
-});
-
-module.exports = pool;
